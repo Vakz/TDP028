c82e83e7c301eaffc178fa52ba3f0ead2407b9f9
Fredrik Jonsén <fredrikjonsen@gmail.com>
2015-12-30 00:52:00 +0100

    Added a DrawerLayout, which functions as a container for the rest of the app. All other activites will have their own view inside the current content-placeholder

---
+++ BeerApp/app/src/main/java/se/liu/student/frejo105/beerapp/DrawerMenuActivity.java
@@ -0,0 +1,29 @@
+package se.liu.student.frejo105.beerapp;
+
+import android.os.Bundle;
+import android.support.annotation.LayoutRes;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.FrameLayout;
+
+public abstract class DrawerMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void setContentView(@LayoutRes int layoutResID) {
+        DrawerLayout container = (DrawerLayout)getLayoutInflater().inflate(R.layout.activity_drawer_menu, null);
+        FrameLayout content = (FrameLayout)findViewById(R.id.contentframe);
+        getLayoutInflater().inflate(layoutResID, content, true);
+        super.setContentView(container);
+        Toolbar toolbar = (Toolbar) container.findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+}
