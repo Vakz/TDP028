df39e6f0dd4c40b236810edd2a9e694e61924cb1
Fredrik Jonsén <fredrikjonsen@gmail.com>
2016-01-02 10:37:28 +0100

    Added toggle button for drawer menu

--- BeerApp/app/src/main/java/se/liu/student/frejo105/beerapp/API/RequestCompleteCallback.java
+++ BeerApp/app/src/main/java/se/liu/student/frejo105/beerapp/API/RequestCompleteCallback.java
@@ -8,6 +8,6 @@ import cz.msebera.android.httpclient.client.HttpResponseException;
  * Created by vakz on 12/28/15.
  */
 public interface RequestCompleteCallback<T> {
-    public void onSuccess(T result);
-    public void onFailure(HttpResponseException hre);
+    void onSuccess(T result);
+    void onFailure(HttpResponseException hre);
 }
--- BeerApp/app/src/main/java/se/liu/student/frejo105/beerapp/DrawerMenuActivity.java
+++ BeerApp/app/src/main/java/se/liu/student/frejo105/beerapp/DrawerMenuActivity.java
@@ -2,9 +2,9 @@ package se.liu.student.frejo105.beerapp;
 
 import android.os.Bundle;
 import android.support.annotation.LayoutRes;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -19,11 +19,13 @@ public abstract class DrawerMenuActivity extends AppCompatActivity {
 
     @Override
     public void setContentView(@LayoutRes int layoutResID) {
-        DrawerLayout container = (DrawerLayout)getLayoutInflater().inflate(R.layout.activity_drawer_menu, null);
+        final DrawerLayout container = (DrawerLayout)getLayoutInflater().inflate(R.layout.activity_drawer_menu, null);
         FrameLayout content = (FrameLayout)findViewById(R.id.contentframe);
         getLayoutInflater().inflate(layoutResID, content, true);
         super.setContentView(container);
         Toolbar toolbar = (Toolbar) container.findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        ActionBarDrawerToggle toggler = new ActionBarDrawerToggle(this, container, toolbar, R.string.drawer_toggle_open, R.string.drawer_toggle_close);
+        container.setDrawerListener(toggler);
     }
 }
