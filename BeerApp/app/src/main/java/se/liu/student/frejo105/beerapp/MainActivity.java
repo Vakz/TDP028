06b0c5a3d098b113e9de8f20933ef1cdd71f1ebf
Fredrik Jonsén <fredrikjonsen@gmail.com>
2016-06-19 11:38:58 +0200

    Moved old project out. Added base for new version.

---
+++ BeerApp/app/src/main/java/se/liu/student/frejo105/beerapp/MainActivity.java
@@ -0,0 +1,13 @@
+package se.liu.student.frejo105.beerapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
